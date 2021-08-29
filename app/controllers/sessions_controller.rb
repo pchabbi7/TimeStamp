3624d0d8a38dbd6b385ead576ae3b995d6b1c5da
pchabbi7 <pri.17feb@gmail.com>
2021-08-29 16:37:36 -0600

    Removed the debugger code and cross sign from the alert banner on the login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,6 @@ class SessionsController < ApplicationController
   end
 
   def create
-   binding.pry
     @user = User.find_by(username: params["user"]["login"])
     if @user 
        $session_user_id = @user.id
