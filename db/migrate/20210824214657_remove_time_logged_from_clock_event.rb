681eb01e4309a853dcbac36e4ea3b56b6a99c427
pchabbi7 <pri.17feb@gmail.com>
2021-08-29 00:25:25 -0600

    Removed unwanted migration file

--- db/migrate/20210824214657_remove_time_logged_from_clock_event.rb
+++
@@ -1,6 +0,0 @@
-class RemoveTimeLoggedFromClockEvent < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :clock_events, :time_logged, :datetime
-    remove_column :clock_events, :id, :integer
-  end
-end
--- db/migrate/20210824220653_add_id_to_clock_event.rb
+++ db/migrate/20210824220653_add_id_to_clock_event.rb
@@ -1,5 +1,5 @@
 class AddIdToClockEvent < ActiveRecord::Migration[6.1]
   def change
-    add_column :clock_events, :id, :primary_key
+    # add_column :clock_events, :id, :primary_key
   end
 end
